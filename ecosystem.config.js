addba35bf6b8cf0fb3df3394a53fa26800a1720c
unknown <jotsamikael@gmail.com>
2026-05-31 02:42:00 +0100

    Add version to backoffice footer

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,15 @@ module.exports = {
   apps: [{
     name: 'gohappygo-api',
     script: 'dist/main.js',
+    cwd: '/home/mikael/htdocs/api.gohappygo.fr/current',
+    env_file: '/home/mikael/htdocs/api.gohappygo.fr/shared/.env.production',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000
+      PORT: 3000,
     },
-    env_file: '.env.production'
-  }]
+  }],
 };
